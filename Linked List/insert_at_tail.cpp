12ed8c377a1c46968b36f97543fa4337594b9e39
Joynal Abedin <cfcfahim26@gmail.com>
2023-09-06 12:42:15 +0600

    singly linked list insert at tail

--- Linked List/insert_at_tail.cpp
+++ Linked List/insert_at_tail.cpp
@@ -1,51 +1,74 @@
 #include <iostream>
 using namespace std;
-
-class Node {
+class Node
+{
 public:
   int data;
   Node *next;
 
-  Node(int data)
+  Node(int val)
   {
-    this->data = data;
+    this->data = val;
     this->next = NULL;
   }
 };
 
-void insertAtTail(Node *&head, int val){
+void insertAtTail(Node *&head, int val)
+{
   Node *newNode = new Node(val);
+
   if (head == NULL)
   {
     head = newNode;
     return;
   }
+
   Node *temp = head;
+
   while (temp->next != NULL)
   {
     temp = temp->next;
   }
   temp->next = newNode;
+  return;
 }
-
-void printLinkedList(Node *head){
+void printLinkedList(Node *head)
+{
   Node *temp = head;
+  cout << "The linked list" << endl;
   while (temp != NULL)
   {
-    cout << temp->data << " ";
+    cout << temp->data << endl;
     temp = temp->next;
   }
 }
-
-int main(){
+int main()
+{
   Node *head = NULL;
-  cout << "Option 1: Insert at tail" << endl;
-  int op;
-  cin >> op;
-  if (op == 1)
+  while (true)
   {
-    insertAtTail(head, 5);
+    cout << "Option 1: Insert at tail" << endl;
+    cout << "Option 2: Print linked list" << endl;
+    cout << "Option 2: Exit" << endl;
+    int op;
+    cin >> op;
+
+    if (op == 1)
+    {
+      cout << "Enter value you want to insert at tail" << endl;
+      int val;
+      cin >> val;
+      insertAtTail(head, val);
+    }
+    else if (op == 2)
+    {
+      printLinkedList(head);
+    }
+    else if (op == 3)
+    {
+      break;
+    }
   }
-  printLinkedList(head);
+
   return 0;
 }
